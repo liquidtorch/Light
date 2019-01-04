7f3d856c017b2daa67cc2a8f1e11e0520b5e9f92
Andy Zalit <andyzalit@gmail.com>
2019-01-04 10:12:33 -0700

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Andy Zalit on 1/4/19.
+//  Copyright © 2019 Andy Zalit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
